65606296e80b0ccc79b7a43aa0b72e7ff8fa83bc
HendraMaajid <hendralatiefulm@gmail.com>
2024-11-03 20:33:36 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lama',
+  webDir: 'www'
+};
+
+export default config;
